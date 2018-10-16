0be8f35b479c2d3f784293ec785c38b6e6fa46c2
wanghaoming <whimmy@126.com>
2018-10-16 15:43:05 +0800

    change to logback

--- spring-statemachine-intro/src/main/java/com/ymmihw/spring/statemachine/config/StateMachineListener.java
+++ spring-statemachine-intro/src/main/java/com/ymmihw/spring/statemachine/config/StateMachineListener.java
@@ -1,12 +1,13 @@
 package com.ymmihw.spring.statemachine.config;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 public class StateMachineListener<S, E> extends StateMachineListenerAdapter<S, E> {
 
-  public static final Logger LOGGER = Logger.getLogger(StateMachineListener.class.getName());
+  public static final Logger LOGGER = LoggerFactory.getLogger(StateMachineListener.class.getName());
 
   @Override
   public void stateChanged(State<S, E> from, State<S, E> to) {
